a458f78e2012a9fa0813c808f91308f022e00c2c
Lerew <lerewz@oregonstate.edu>
2017-05-19 12:09:33 -0700

    Added template and a couple pages of tutorial work

---
+++ mvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        public ActionResult Index()
+        {
+            //return "This is my <b>default</b> action...";
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public ActionResult Welcome(String name, int numTimes = 1) {
+            //return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View(); //(ViewBag)
+        }
+    }
+}
---
+++ mvcMovie/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using System.Data.Entity;
+
+namespace mvcMovie.Models
+{
+    public class Movie
+    {
+        // Members
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+
+
+        // Accessors and Mutators
+
+
+    }
+
+    public class MovieDBContext : DbContext
+    {
+        // Members
+        public DbSet<Movie> Movies { get; set; }
+
+
+
+    }
+}
