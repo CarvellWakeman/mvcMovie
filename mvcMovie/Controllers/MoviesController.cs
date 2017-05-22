f8f42b3c44162fce99f9ba6742c955b74c315776
Lerew <lerewz@oregonstate.edu>
2017-05-22 09:47:13 -0700

    Finished mvcMovie tutorial

--- mvcMovie/Controllers/MoviesController.cs
+++ mvcMovie/Controllers/MoviesController.cs
@@ -115,15 +115,15 @@ namespace mvcMovie.Controllers
         // GET: Movies/Delete/5
         public ActionResult Delete(int? id)
         {
-            if (id == null)
-            {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
+
             Movie movie = db.Movies.Find(id);
-            if (movie == null)
-            {
+            if (movie == null) {
                 return HttpNotFound();
             }
+
             return View(movie);
         }
 
